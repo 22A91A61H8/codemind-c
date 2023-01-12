53107619a7b72fe4eeaf8a8fc0a0c38b8b3c6795
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-01-12 16:33:45 +0530

    #include <stdio.h> int happy(int num) {     int rem=0,sum=0;     while(num>0)     {         rem = num % 10;         sum = sum + (rem * rem);         num = num / 10;     }     return sum; } int main() {     int num;     scanf("%d",&num);     int result=num;     while(result != 1 && result != 4)     {         result=happy(result);     }     if(result==1)     {         printf("True");     }     else if(result==4)     {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int happy(int num)
+{
+    int rem=0,sum=0;
+    while(num>0)
+    {
+        rem = num % 10;
+        sum = sum + (rem * rem);
+        num = num / 10;
+    }
+    return sum;
+}
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int result=num;
+    while(result != 1 && result != 4)
+    {
+        result=happy(result);
+    }
+    if(result==1)
+    {
+        printf("True");
+    }
+    else if(result==4)
+    {
+        printf("False");
+    }
+}
