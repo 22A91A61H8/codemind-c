eca8e4698c890968f5055fdd32e8817e0fd907f1
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-01-12 15:19:18 +0530

    #include <stdio.h> int main() {     int n,temp,sum=0,product=1,last_digit;     scanf("%d",&n);     temp=n;     while(n>0)     {         last_digit=n%10;         sum=sum+last_digit;         product=product*last_digit;         n=n/10;     }     if(sum==product)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

---
+++ Spy_Number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n,temp,sum=0,product=1,last_digit;
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        last_digit=n%10;
+        sum=sum+last_digit;
+        product=product*last_digit;
+        n=n/10;
+    }
+    if(sum==product)
+    {
+        printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
+}
