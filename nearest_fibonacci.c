094ffb411ab3174afcf364794ce7641994ae5eb5
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-01-27 10:10:10 +0530

    #include <stdio.h> #include <math.h> int main() {     int n;     scanf("%d",&n);     int a=0,b=1,c;     for(int i=1;i<=n;i++)     {         c=a+b;         if(c>n)         {             break;         }         a=b;         b=c;     }     int d1=abs(n-c);     int d2=abs(n-b);     if(d1>d2)     {         printf("%d",b);     }     else if(d1<d2)     {         printf("%d",c);     }     else     {         printf("%d %d",b,c);     } }

---
+++ nearest_fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=0,b=1,c;
+    for(int i=1;i<=n;i++)
+    {
+        c=a+b;
+        if(c>n)
+        {
+            break;
+        }
+        a=b;
+        b=c;
+    }
+    int d1=abs(n-c);
+    int d2=abs(n-b);
+    if(d1>d2)
+    {
+        printf("%d",b);
+    }
+    else if(d1<d2)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("%d %d",b,c);
+    }
+}
