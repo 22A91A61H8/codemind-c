f3fb115576bd43ec32a3965d6b684fbdecf58dff
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-05-24 12:09:25 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n;i>=1;i--)     {         for(int j=1;j<=i-1;j++)         {             printf(" ");         }         for(int k=1;k<=n;k++)         {            if(i==1 || i==n || k==1 || k==n)             printf("*");             else             printf(" ");         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,67 +1,23 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            if(i==1)
-            printf("A ");
-            else if(i==2)
-            printf("B ");
-            else if(i==3)
-            printf("C ");
-            else if(i==4)
-            printf("D ");
-            else if(i==5)
-            printf("E ");
-            else if(i==6)
-            printf("F ");
-            else if(i==7)
-            printf("G ");
-            else if(i==8)
-            printf("H ");
-            else if(i==9)
-            printf("I ");
-            else if(i==10)
-            printf("J ");
-            else if(i==11)
-            printf("K ");
-            else if(i==12)
-            printf("L ");
-            else if(i==13)
-            printf("M ");
-            else if(i==14)
-            printf("N ");
-            else if(i==15)
-            printf("O ");
-            else if(i==16)
-            printf("P ");
-            else if(i==17)
-            printf("Q ");
-            else if(i==18)
-            printf("R ");
-            else if(i==19)
-            printf("S ");
-            else if(i==20)
-            printf("T ");
-            else if(i==21)
-            printf("U ");
-            else if(i==22)
-            printf("V ");
-            else if(i==23)
-            printf("W ");
-            else if(i==24)
-            printf("X ");
-            else if(i==25)
-            printf("Y ");
-            else if(i==26)
-            printf("Z ");
-            }
-            printf("
-");
-    }
-    return 0;
+int main()  
+{  
+    int n;  
+    scanf("%d",&n);  
+    for(int i=n;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+            printf(" ");  
+        }  
+        for(int k=1;k<=n;k++)  
+        {  
+           if(i==1 || i==n || k==1 || k==n)  
+            printf("*");  
+            else  
+            printf(" ");   
+        }  
+        printf("
+");  
+    }  
+    return 0;  
 }
