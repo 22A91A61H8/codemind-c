864d35978cecdbb9ed5793fede8066c39208ee90
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-05-24 12:10:30 +0530

    #include<stdio.h> int main() {     int n,a;     scanf("%d",&n);     n--;     for(int i=n;i>=0;i--)     {         a=i+65;         for(int j=0;j<=i;j++)         {             printf("%c ",a);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
-int main()  
-{  
-    int n;  
-    scanf("%d",&n);  
-    for(int i=n;i>=1;i--)  
-    {  
-        for(int j=1;j<=i-1;j++)  
-        {  
-            printf(" ");  
-        }  
-        for(int k=1;k<=n;k++)  
-        {  
-           if(i==1 || i==n || k==1 || k==n)  
-            printf("*");  
-            else  
-            printf(" ");   
-        }  
+int main()
+{
+    int n,a;
+    scanf("%d",&n);
+    n--;
+    for(int i=n;i>=0;i--)
+    {
+        a=i+65;
+        for(int j=0;j<=i;j++)
+        {
+            printf("%c ",a);
+        }
         printf("
-");  
-    }  
-    return 0;  
+");
+    }
 }
